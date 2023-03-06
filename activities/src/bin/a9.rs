6ac9e800be1e1d7005939d6326dc261a9cf2f1b3
todak-sun <tjsdydwn@gmail.com>
2023-03-06 22:06:46 +0900

    feat: 튜플 사용해보기

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -5,8 +5,22 @@
 //   greater than 5, less than 5, or equal to 5
 //
 // Notes:
-// * Use a function that returns a tuple
-// * Destructure the return value into two variables
-// * Use an if..else if..else block to determine what to print
 
-fn main() {}
+// * Use a function that returns a tuple
+fn coords() -> (i32, i32) {
+    (1, 7)
+}
+
+fn main() {
+    // * Destructure the return value into two variables
+    let (_, y) = coords();
+
+    // * Use an if..else if..else block to determine what to print
+    if y > 5 {
+        println!(">5")
+    } else if y < 5 {
+        println!("<5")
+    } else {
+        println!("=5")
+    }
+}
