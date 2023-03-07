4b29947a3bcc5b56ddca450b1711517d03ce2aac
todak-sun <tjsdydwn@gmail.com>
2023-03-07 21:54:00 +0900

    feat: 소유권 예제 까지 수강 완료

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -5,11 +5,24 @@
 // * Print "its small" if a variable is <= 100
 //
 // Notes:
+
+// * Use a function to print the messages
+fn print_message(result: bool) {
+  // * Use a match expression to determine which message
+  //   to print
+  match result {
+    true => println!("It's big"),
+    false => println!("It's small")
+  }
+}
+
 // * Use a boolean variable set to the result of
 //   an if..else expression to store whether the value
 //   is > 100 or <= 100
-// * Use a function to print the messages
-// * Use a match expression to determine which message
-//   to print
+fn main() {
+  let num: i32 = 50;
+  let result: bool = num > 100;
 
-fn main() {}
+  println!("{:?}", result);
+  print_message(result)
+}
--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -3,10 +3,33 @@
 // Requirements:
 // * Print out the quantity and id number of a grocery item
 //
+
+
+
 // Notes:
 // * Use a struct for the grocery item
 // * Use two i32 fields for the quantity and id number
-// * Create a function to display the quantity, with the struct as a parameter
-// * Create a function to display the id number, with the struct as a parameter
+struct GroceryItem {
+    id: i32,
+    quantity: i32,
+}
 
-fn main() {}
+// * Create a function to display the quantity, with the struct as a parameter
+fn display_quantity(item: &GroceryItem) {
+  println!("{:?}", item.quantity);
+}
+
+// * Create a function to display the id number, with the struct as a parameter
+fn display_id(item: &GroceryItem) {
+  println!("{:?}", item.id);
+}
+
+fn main() {
+  let item = GroceryItem{
+    id: 1,
+    quantity: 10
+  };
+
+  display_id(&item);
+  display_quantity(&item);
+}
