64c0e59bb0c542f5acd582983289ff7fbfa1f6cf
todak-sun <tjsdydwn@gmail.com>
2023-03-09 23:14:58 +0900

    feat: Option<T> 까지 수강완료

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -8,7 +8,33 @@
 //
 // Notes:
 // * Use an enum for the tickets with data associated with each variant
+
+#[derive(Debug)]
+enum Ticket {
+    Backstage(f64, String),
+    Vip(f64, String),
+    Standard(f64),
+}
+
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+fn main() {
+    let tickets = vec![
+        Ticket::Backstage(35.2, "Todak".to_owned()),
+        Ticket::Vip(32.2, "Todak".to_owned()),
+        Ticket::Standard(20.4),
+    ];
+
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, holder) => {
+                println!("Backstage ticket Holder : {:?}, price: {:?}", holder, price)
+            }
+            Ticket::Standard(price) => println!("Standard ticket Price: {:?}", price),
+            Ticket::Vip(price, holder) => {
+                println!("VIP Ticekt Holder : {:?}, price: {:?}", holder, price)
+            }
+        }
+    }
+}
--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -7,8 +7,31 @@
 // Notes:
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
+struct Student {
+    name: String,
+    locker: Option<i32>,
+}
 
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
+fn main() {
+    let students = vec![
+        Student {
+            name: "Todak".to_owned(),
+            locker: Some(1324),
+        },
+        Student {
+            name: "Hong".to_owned(),
+            locker: None,
+        },
+        Student {
+            name: "Kim".to_owned(),
+            locker: Some(1324),
+        },
+    ];
 
-fn main() {}
+    for student in students {
+      match student.locker {
+          Some(assignment) => println!("Student [{:?}] has assignment. - {:?}", student.name, assignment),
+          None => println!("Student [{:?}] dosen't have assignment", student.name)
+      }
+    }
+}
