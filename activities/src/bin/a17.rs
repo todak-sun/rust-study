366bb901f38b79aa1352d7d4b4e306f8cdbfc72c
todak-sun <tjsdydwn@gmail.com>
2023-03-12 20:09:36 +0900

    문서화...

--- activities/src/bin/a17.rs
+++ activities/src/bin/a17.rs
@@ -12,4 +12,7 @@
 //   to uppercase and lowercase
 //   * Try searching for: to_uppercase, to_lowercase
 
-fn main() {}
+fn main() {
+  let name = "todak";
+  println!("uppercase : {:?}", name.to_uppercase()) ;
+}
